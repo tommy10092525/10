88e21d04c1476d94a844d0ed9bde503a675778a2
tommy10092525 <YOUR_EMAIL>
2024-10-09 18:30:10 +0900

    bootstrapの追加

--- features.py
+++ features.py
@@ -1,8 +1,9 @@
 import sqlite3
 from datetime import datetime
+from typing import Any
 
 
-def get_posts(cur: sqlite3.Cursor):
+def get_posts(cur: sqlite3.Cursor) -> list[dict[str, Any]]:
     result = cur.execute("""
         SELECT content,created_at
         FROM posts
@@ -12,10 +13,10 @@ def get_posts(cur: sqlite3.Cursor):
     posts = [dict(content=item[0], created_at=item[1])for item in result]
     return posts
 
-def set_post(cur:sqlite3.Cursor,content:str):
+def set_post(cur:sqlite3.Cursor,content:str) -> None:
     if content!="":
         cur.execute("""
-                    INSERT INTO posts(content,created_at)
-                    VALUES(?,?)""",
-                    (content, datetime.now()))
+            INSERT INTO posts(content,created_at)
+            VALUES(?,?)""",
+            (content, datetime.now()))
         cur.commit()
