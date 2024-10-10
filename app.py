da46b65488b84fdce70ac524b923b3e72d652b47
tommy10092525 <YOUR_EMAIL>
2024-10-11 02:06:54 +0900

    DBの基礎的な機能が完成

--- app.py
+++ app.py
@@ -1,25 +1,47 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 from datetime import datetime
-from features.db import dictFactory, getThreads,getDivisions,getKindsByDivisionsId
+import features.db as db
+from features.db import addThread,addResponse
 from pprint import pprint
 
 app = Flask(__name__)
 path = "db.sqlite3"
 con = sqlite3.connect(database=path, check_same_thread=False)
-con.row_factory = dictFactory
+con.row_factory = db.dictFactory
 cur = con.cursor()
 
 
 @app.route("/", methods=["GET"])
 def index():
     data = dict(threads=[],head=[])
-    data["threads"]=getThreads(cur)
-    for division in getDivisions(cur):
-        kinds=getKindsByDivisionsId(cur,division["id"])
+    data["threads"]=db.getThreads(cur)
+    for division in db.getDivisions(cur):
+        kinds=db.getKindsByDivisionsId(cur,division["id"])
         data["head"].append(dict(division=division,kinds=kinds))
     pprint(data)
     return render_template("index.html", data=data)
 
+@app.route("/kind/<int:kind_id>",methods=["GET","POST"])
+def threads(kind_id):
+    if request.method=="GET":
+        data=dict(threads=db.getThreadsByKindId(cur,kind_id))
+        print(data)
+        return render_template("threads.html",data=data)
+    else:
+        title=request.form["title"]
+        content=request.form["content"]
+        threadId=addThread(cur=cur,title=title,kindId=kind_id)
+        addResponse(cur=cur,content=content,threadId=threadId)
+        con.commit()
+        return redirect(f"/{kind_id}")
+
+@app.route("/thread/<int:thread_id>",methods=["GET","POST"])
+def thread(thread_id):
+    if request.method=="GET":
+        data=dict(thread=db.getThreadById(cur,thread_id),responses=db.getResponsesByThreadId(cur,thread_id))
+        return render_template("thread.html",data=data)
+        
+        
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
--- features/db.py
+++ features/db.py
@@ -3,12 +3,20 @@ from datetime import datetime
 from typing import Any
 from features.time_handler import generateTimeCaption
 
-def dateConverter(result:list[dict[str,Any]]) -> list[dict[str, Any]]:
-    for i in result:
-        for j in i.keys():
-            if j=="created_at":
-                i[j]=generateTimeCaption(i[j])
+
+def dateConverter(result: list[dict[str, Any]] | dict[str, Any]) -> list[dict[str, Any]]:
+    print(type(result))
+    if type(result) is list:
+        for i in result:
+            for j in i.keys():
+                if j == "created_at":
+                    i[j] = generateTimeCaption(i[j])
+    else:
+        for j in result.keys():
+            if j == "created_at":
+                result[j] = generateTimeCaption(result[j])
     return result
+
 
 def dictFactory(cur: sqlite3.Cursor, row):
     d = {}
@@ -16,66 +24,69 @@ def dictFactory(cur: sqlite3.Cursor, row):
         d[col[0]] = row[idx]
     return d
 
-def getThreads(cur:sqlite3.Cursor) -> list[dict[str, Any]]:
-    result=cur.execute("""--sql
+
+def getThreadById(cur: sqlite3.Cursor, id: int):
+    result = cur.execute("""--sql:
+        select *
+        from threads
+        where id=?""", (id,)).fetchone()
+    return dateConverter(result)
+
+
+def getThreads(cur: sqlite3.Cursor) -> list[dict[str, Any]]:
+    result = cur.execute("""--sql
         select *
         from threads
         order by created_at
         limit 10""").fetchall()
-    
+
     return dateConverter(result)
 
-def getThreadsByKindId(cur:sqlite3.Cursor,id:int) -> list[dict[str, Any]]:
-    result=cur.execute("""--sql
+
+def getThreadsByKindId(cur: sqlite3.Cursor, kindId: int) -> list[dict[str, Any]]:
+    result = cur.execute("""--sql
         select *
         from threads
         where kind_id=?
-        limit 100""",(id,)).fetchall()
+        limit 100""", (kindId,)).fetchall()
     return dateConverter(result)
-    
-def getDivisions(cur:sqlite3.Cursor) -> list[dict[str, Any]]:
-    result=cur.execute("""--sql
+
+
+def getDivisions(cur: sqlite3.Cursor) -> list[dict[str, Any]]:
+    result = cur.execute("""--sql
         select *
         from divisions""").fetchall()
     return dateConverter(result)
-    
-def getKindsByDivisionsId(cur:sqlite3.Cursor,divisionsId:int) -> list[dict[str, Any]]:
-    result=cur.execute("""--sql
+
+
+def getKindsByDivisionsId(cur: sqlite3.Cursor, divisionsId: int) -> list[dict[str, Any]]:
+    result = cur.execute("""--sql
         select *
         from kinds
-        where division_id=?""",(divisionsId,)).fetchall()
+        where division_id=?""", (divisionsId,)).fetchall()
     return dateConverter(result)
 
-# def getPosts(cur: sqlite3.Cursor) -> list[dict[str, Any]]:
-#     result = cur.execute("""--sql
-#         SELECT id,content,created_at
-#         FROM posts
-#         ORDER BY created_at DESC
-#         LIMIT 100""").fetchall()
-#     return result
+
+def getResponsesByThreadId(cur: sqlite3.Cursor, threadId):
+    result = cur.execute("""--sql
+        select *
+        from responses
+        where thread_id=?""", (threadId,)).fetchall()
+    return dateConverter(result)
 
 
-# def setPost(cur: sqlite3.Cursor, content: str) -> None:
-#     if content != "" and len(content) <= 1000:
-#         cur.execute("""--sql
-#             INSERT INTO posts(content,created_at)
-#             VALUES(?,?)""",
-#                     (content, datetime.now()))
+def addThread(cur: sqlite3.Cursor, title: str, kindId: int):
+    cur.execute("""--sql
+        insert into threads(title,kind_id,created_at)
+        values(?,?,?)""", (title, kindId, datetime.now()))
+    result = cur.execute("""--sql
+        select max(id)
+        as max
+        from threads""").fetchone()["max"]
+    return dateConverter(result)
 
 
-# def getPostById(cur: sqlite3.Cursor, id: int):
-#     result = cur.execute("""--sql
-#         SELECT id,content,created_at
-#         FROM posts
-#         WHERE id=?""", (id,)).fetchone()
-#     return result
-
-
-# def getPostCount(cur: sqlite3.Cursor):
-#     result = cur.execute("""--sql
-#         SELECT COUNT(*)
-#         AS count
-#         FROM posts""").fetchone()
-#     return result["count"]
-
-
+def addResponse(cur: sqlite3.Cursor, content: str, threadId: int):
+    cur.execute("""--sql
+        insert into responses(content,thread_id,created_at)
+        values(?,?,?)""", (content, threadId, datetime.now()))
